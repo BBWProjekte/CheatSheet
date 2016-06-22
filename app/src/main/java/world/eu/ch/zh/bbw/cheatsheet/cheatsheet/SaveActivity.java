19a089128afea0ee1108057e841d62d72e5344c6
KajTorvaldGrey <kajtorvaldgrey@googlemail.com>
2016-06-22 11:22:18 +0200

    updated saveactivity

--- app/src/main/java/world/eu/ch/zh/bbw/cheatsheet/cheatsheet/SaveActivity.java
+++ app/src/main/java/world/eu/ch/zh/bbw/cheatsheet/cheatsheet/SaveActivity.java
@@ -2,15 +2,21 @@ package world.eu.ch.zh.bbw.cheatsheet.cheatsheet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by janes_000 on 22.06.2016.
  */
-public class SaveActivity extends Activity{
+public class SaveActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+
 }

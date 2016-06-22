807cff81ca5768354e9ea2e06ba314a82caef7c7
Matti <DiniMrHetEnSchwanz@Winti.ch>
2016-06-22 13:11:28 +0200

    SaveActivity: GetLocation

--- app/src/main/java/world/eu/ch/zh/bbw/cheatsheet/cheatsheet/SaveActivity.java
+++ app/src/main/java/world/eu/ch/zh/bbw/cheatsheet/cheatsheet/SaveActivity.java
@@ -1,14 +1,30 @@
 package world.eu.ch.zh.bbw.cheatsheet.cheatsheet;
 
 import android.app.Activity;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-/**
- * Created by janes_000 on 22.06.2016.
- */
-public class SaveActivity extends AppCompatActivity{
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class SaveActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
+    public static final String TAG = SaveActivity.class.getSimpleName();
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +32,77 @@ public class SaveActivity extends AppCompatActivity{
         setContentView(R.layout.activity_save);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
 
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "No permissions!");
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            Log.i(TAG, "location is null");
+        }
+        else
+        {
+            try {
+                handleNewLocation(location);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleNewLocation(Location location) throws IOException {
+        Log.d(TAG + ": Latitude", String.valueOf(location.getLatitude()));
+        Log.d(TAG + ": Longitude", String.valueOf(location.getLongitude()));
+        Log.d(TAG + ": Accuracy", String.valueOf(location.getAccuracy()));
+
+        Geocoder gcd = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = gcd.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        if (addresses.size() > 0)
+            Log.d(TAG + ": City", addresses.get(0).getLocality());
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
 }

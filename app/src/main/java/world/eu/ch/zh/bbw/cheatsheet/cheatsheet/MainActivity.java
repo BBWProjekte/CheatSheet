3e576fc0d0ab1a8d8652f72423d543ea27f6e75b
KajTorvaldGrey <kajtorvaldgrey@googlemail.com>
2016-06-22 11:29:59 +0200

    thank god, you can see activity_save now

--- app/src/main/java/world/eu/ch/zh/bbw/cheatsheet/cheatsheet/MainActivity.java
+++ app/src/main/java/world/eu/ch/zh/bbw/cheatsheet/cheatsheet/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), SaveActivity.class);
+                startActivityForResult(intent,0);
             }
         });
     }
